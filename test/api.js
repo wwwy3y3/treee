c073b5d7aeef17a27f0fa82f51d5867048225845
william <wwwy3y3@gmail.com>
2015-01-16 15:57:49 +0800

    realtive path opts

--- test/api.js
+++ test/api.js
@@ -1,7 +1,7 @@
 var treee= require('../');
 var jsome= require('jsome');
 
-treee.get(process.cwd()+'/test/folder', { skipDotDir: true })
+treee.get(process.cwd()+'/test/folder', { nodeName: 'label', skipDotDir: true, showRelative: true })
 	 .done(function (arr) {
 	 	jsome(arr);
 	 }, function (err) {
