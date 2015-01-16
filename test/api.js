62a3770d783e824afb5f997abcacec6322e7b184
william <wwwy3y3@gmail.com>
2015-01-16 15:49:51 +0800

    dot dir skip

--- test/api.js
+++ test/api.js
@@ -1,7 +1,7 @@
 var treee= require('../');
 var jsome= require('jsome');
 
-treee.get(process.cwd()+'/test/folder')
+treee.get(process.cwd()+'/test/folder', { skipDotDir: true })
 	 .done(function (arr) {
 	 	jsome(arr);
 	 }, function (err) {
